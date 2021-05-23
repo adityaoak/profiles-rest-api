3b8f72e0c40f6175f1a486eba876d539c9b72bfa
Aditya <adioak1992@gmail.com>
2021-05-23 00:30:45 -0600

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -3,3 +3,4 @@ from profiles_api import models  #import our custom class
 
 # Register your models here.
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser  #added new
 from django.contrib.auth.models import PermissionsMixin   #added new
 from django.contrib.auth.models import BaseUserManager   #added new
+from django.conf import settings
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
@@ -40,6 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):  #Singular name since Dja
     # specify model manager we're gonna use
     objects = UserProfileManager()
 
+
     # additional required fields
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
@@ -56,3 +58,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):  #Singular name since Dja
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
